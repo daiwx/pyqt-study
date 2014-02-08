f84c1d4be3b33d8068f353362aa075c88c28d02d
Syohei YOSHIDA <syohex@gmail.com>
2014-02-08 21:20:33 +0900

    add samples

---
+++ r7/qitem_selection_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt4 import QtGui, QtCore
+
+
+class Example(QtGui.QMainWindow):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle("Selection")
+
+        self.initData()
+        self.initUI()
+
+    def initUI(self):
+        self.tv = QtGui.QTableView(self)
+        self.tv.setModel(self.model)
+
+        icon = QtGui.QIcon('sum.png')
+        self.nsum = QtGui.QAction(icon, "Sum ", self)
+        self.nsum.setShortcut('Ctrl+Q')
+        self.connect(self.nsum, QtCore.SIGNAL('triggerd()'), self.onClicked)
+
+        self.toolbar = self.addToolBar('Sum')
+        self.toolbar.addAction(self.nsum)
+
+        self.setCentralWidget(self.tv)
+
+    def onClicked(self):
+        nsum = 0
+
+        selmod = self.tv.selectionModel()
+        selection = selmod.selection()
+        indexes = selection.indexes()
+
+        for idx in indexes:
+            num = idx.data().toInt()
+            nsum += num[0]
+
+        lastIndex = indexes[-1]
+        r, c = lastIndex.row(), lastIndex.column()
+
+        item = QtGui.QStandardItem(str((nsum)))
+        self.model.setItem(r+1, c, item)
+
+    def initData(self):
+        self.model = QtGui.QStandardItemModel(15, 15)
+
+
+app = QtGui.QApplication([])
+ex = Example()
+ex.show()
+app.exec_()
